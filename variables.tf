85ccef2d41be3133579961346b1e7549d6434840
mayursangewar <sangewarmayur7664@gmail.com>
2026-04-27 00:11:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,31 @@
 variable "instance_name" {
-  description = "Name tag for the EC2 instance"
-  type        = string
+  type = string
 }
 
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
+  type = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+  type = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID where the instance will be launched"
-  type        = string
+  type = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID for the security group"
-  type        = string
+  type = string
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block used to restrict SSH in prod"
-  type        = string
-  default     = "10.0.0.0/8"
+  type = string
 }
 
 variable "environment" {
-  description = "Environment name (dev, uat, prod)"
-  type        = string
+  type = string
 }
 
 variable "tags" {
-  description = "Map of tags to apply to all resources"
-  type        = map(string)
-  default     = {}
+  type = map(string)
 }
